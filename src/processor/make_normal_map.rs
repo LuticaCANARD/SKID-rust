5160487a06036c3981a8d2e0c639a987823b720a
Lutica_CANARD <presan100@gmail.com>
2025-06-04 03:18:10 +0900

    모듈 수정: make_normal_map_base 함수의 반환 타입을 SKIDImage로 변경하고 구현 추가

--- src/processor/make_normal_map.rs
+++ src/processor/make_normal_map.rs
@@ -3,7 +3,12 @@ use crate::model::skid_image::SKIDImage;
 
 pub fn make_normal_map_base(
     original_image: &SKIDImage,
-) {
-
+) -> SKIDImage {
+    // Placeholder for the actual implementation
+    // This function should create a normal map from the original image
+    // For now, we will just return a new SKIDImage with the same dimensions
+    let size = original_image.get_size();
+    SKIDImage::new(size.width, size.height)
+    
 }
 
