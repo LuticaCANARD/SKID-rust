becb500e8d57ef81f1060ab7a5cac1b57f7bb939
Lutica_CANARD <presan100@gmail.com>
2025-06-07 21:59:43 +0900

    GPU 호출 테스트에서 Vulkan 초기화 추가 및 WgpuRuntime을 사용하여 노멀 맵 생성 로직 개선

--- src/test/gpu_call_tests.rs
+++ src/test/gpu_call_tests.rs
@@ -1,3 +1,5 @@
+use cubecl::wgpu::{Dx12, Vulkan};
+
 use crate::{model::{skid_color::SKIDColor, skid_image::SKIDImage}, processor::make_normal_map, utils::{file_io, gpu_opt}};
 
 
@@ -16,26 +18,31 @@ fn gpu_call_tests() {
 
 #[test]
 fn gpu_normap_tests() {
-
-
+    
     let example_image = file_io::import_from_png("output/test_input.png", Some(4))
         .expect("Failed to load image from file");
     println!("Loaded image: {:?}", example_image.get_size());
-
+    let device = cubecl::wgpu::WgpuDevice::DiscreteGpu(0);
     
-    let _result_image = make_normal_map::make_normal_map_base::<cubecl::cuda::CudaRuntime>(
-        Default::default(),
+    cubecl::wgpu::init_setup::<Vulkan>(
+        &device, cubecl::wgpu::RuntimeOptions{
+            ..Default::default()
+        }
+    );
+
+    let result_image = make_normal_map::make_normal_map_base::<cubecl::wgpu::WgpuRuntime>(
+        device,
         &example_image,
         Some(1.),
         Some(1.),
     );
 
     file_io::export_to_png(
-        &_result_image,
+        &result_image,
         "output/normal_map_output2.png",
         Some(8),
     ).expect("Failed to export normal map image");
-    println!("Result image: {:?}", _result_image.get_size());
-    println!("Normal map first pixel color: {:?}", _result_image.get_pixel(0, 0));
+    println!("Result image: {:?}", result_image.get_size());
+    println!("Normal map first pixel color: {:?}", result_image.get_pixel(0, 0));
 
 }
